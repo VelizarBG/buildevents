d1fcddc4e7494f33d485859521677e9ca64b1c2f
Velizar <31567122+VelizarBG@users.noreply.github.com>
2023-11-06 21:35:11 +0200

    Count Flint and Steel, and bucket placements
    - Use MixinExtras to eliminate variable redundancy

--- src/main/java/velizarbg/buildevents/mixins/BucketItemMixin.java
+++ src/main/java/velizarbg/buildevents/mixins/BucketItemMixin.java
@@ -2,8 +2,9 @@ package velizarbg.buildevents.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.util.ActionResult;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,10 +15,10 @@ import velizarbg.buildevents.data.BuildEventsState;
 
 import static velizarbg.buildevents.BuildEvents.buildEventsState;
 
-@Mixin(BlockItem.class)
-public class BlockItemMixin {
-	@Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/criterion/ItemCriterion;trigger(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)V"))
-	private void onBlockPlaced(CallbackInfoReturnable<ActionResult> cir, @Local BlockPos pos, @Local World world, @Local PlayerEntity player) {
+@Mixin(BucketItem.class)
+public class BucketItemMixin {
+	@Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/criterion/ItemCriterion;trigger(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)V"))
+	private void onBlockPlaced(CallbackInfoReturnable<TypedActionResult<ItemStack>> cir, @Local World world, @Local PlayerEntity player, @Local(ordinal = 2) BlockPos pos) {
 		for (BuildEventsState.BuildEvent event : buildEventsState.placeEvents) {
 			if (event.world() == world && event.box().contains(pos.getX(), pos.getY(), pos.getZ())) {
 				world.getScoreboard().getPlayerScore(player.getEntityName(), event.placeObjective()).incrementScore();
